c5bd5a42c7435cc5f142dafd1dc3fc8a3c4c51e6
Snehasishk <snehasishk23@gmail.com>
2025-06-07 11:39:10 +0530

    add server

--- list.js
+++ list.js
@@ -1,5 +1,11 @@
 const client = require("./client");
 async function init() {
-  const result = await client.lpush("messages", "1");
+  //   await client.lpush("messages", "1");
+  //   await client.lpush("messages", "2");
+  //   await client.lpush("messages", "3");
+  //   await client.lpush("messages", "4");
+  //   await client.lpush("messages", "5");
+  const result = await client.rpop("messages");
+  console.log(result);
 }
 init();
